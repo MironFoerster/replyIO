c12454702bfc5b908b18f453de735591010b1cb3
MironFoerster <miron.foerster@gmx.de>
2023-03-09 14:51:22 +0100

    fix dl urls

--- replyspeedup/FileInOut/views.py
+++ replyspeedup/FileInOut/views.py
@@ -21,18 +21,17 @@ def index(request):
                'path_source': ""}
     in_lines = []
     if ('FileIn' in request.FILES.keys()) and ('problem_id' in request.POST.keys()):
-        print(request.POST)
         problem_path = os.path.join(settings.MEDIA_ROOT, "FileInOut", str(request.POST['problem_id']))
-
-        context['path_out'] = os.path.join(problem_path, "out.txt")
-        context['path_source'] = os.path.join(problem_path, "source.txt")
+        download_path = os.path.join("http://", request.get_host(), "media", "FileInOut", str(request.POST['problem_id']))
+        context['path_out'] = os.path.join(download_path, "out.txt")
+        context['path_source'] = os.path.join(download_path, "source.txt")
 
         for i in request.FILES['FileIn']:
             in_lines.append(i.decode('utf-8')[:-2])  #.split())  # z.B.: [['4'], ['1234', '7777'], ['1234', '4321'], ['helllo', 'strang', 'err']]
         in_lines.reverse()
 
         out_lines = []
-        exec(open(os.path.join(problem_path, "run.py")).read())
+        exec(open(os.path.join(problem_path, "run.py")).read(), {"in_lines": in_lines, "out_lines": out_lines})
 
         with open(os.path.join(problem_path, "out.txt"), 'w') as file_out:
             file_out.writelines(out_lines)
