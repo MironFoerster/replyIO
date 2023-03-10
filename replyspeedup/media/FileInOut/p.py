a64a93ca4e569fe033de20ee7d6106670a6bdd30
MironFoerster <miron.foerster@gmx.de>
2023-03-10 16:26:36 +0100

    add solution template

---
+++ replyspeedup/media/FileInOut/p.py
@@ -0,0 +1,15 @@
+def xytoid(xy, w, h):
+    return xy[1] * w + xy[0]
+
+def idtoxy(id, w):
+    return [id % w, id // w]
+
+
+in_lines = """"""
+
+for t in range(int(in_lines.pop())):
+    out_line = ""
+
+    out_lines.append(f"Case #{str(t+1)}: {out_line}\n")
+
+# dont delete this line
